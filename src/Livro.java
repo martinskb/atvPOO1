7ff993cbdaaa4d3a94ab46dfa1cc14cdb9906e19
kaio <kaio22barra@gmail.com>
2025-08-24 18:54:12 -0300

    Update Livro.java

--- src/Livro.java
+++ src/Livro.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
+class Livro {
+    String titulo;
+    int numeroPaginas;
+    int anoPublicacao;
+    double preco;
 
-public class Livro {
+    public void cadastrar() {
+        System.out.println("Livro cadastrado: " + titulo);
+    }
+
+    public void vender() {
+        System.out.println("Livro vendido: " + titulo + " por R$" + preco);
+    }
+
+    public void reembolso() {
+        System.out.println("Livro Reembolsado: " + titulo + " Valor Reembolsado: R$" + preco);
+    }
 }
+
