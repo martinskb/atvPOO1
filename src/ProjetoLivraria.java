21d12a6bf1dda371e29b7529d991f811bc825245
kaio <kaio22barra@gmail.com>
2025-08-24 18:10:38 -0300

    Initial commit

---
+++ src/ProjetoLivraria.java
@@ -0,0 +1,24 @@
+public class ProjetoLivraria {
+    public static void main(String[] args) {
+
+        Livro livro1 = new Livro();
+        Livro livro2 = new Livro();
+
+        // Acesso aos atributos (seria melhor usar métodos setters)
+        livro1.titulo = "Senhor dos Mistérios, Vol. 1: O Palhaço, Parte 1";
+        livro1.numeroPaginas = 580;
+        livro1.anoPublicacao = 2025;
+        livro1.preco = 137;
+
+        livro2.titulo = "O Ponto de Vista do Leitor, Vol. 1";
+        livro2.numeroPaginas = 260;
+        livro2.anoPublicacao = 2025;
+        livro2.preco = 93;
+
+        System.out.println("Livro1: " + livro1.titulo + " - " + livro1.preco);
+        System.out.println("Livro2: " + livro2.titulo + " - " + livro2.preco);
+
+        livro1.cadastrar();
+        livro2.vender();
+    }
+}
