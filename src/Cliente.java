0206bf46258b3809a9676ee481d334c26076f1a8
kaio <kaio22barra@gmail.com>
2025-08-24 18:54:40 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
+class Cliente {
+    int id;
+    String nome;
+    String email;
+    String assinatura;
 
-public class Cliente {
+    public void cadastrar() {
+        System.out.println("Cliente cadastrado: ID: " + id + " Nome: " + nome + " Email: " + email);
+    }
+
+    public void verificarAssinatura() {
+        if (assinatura.equals("sim")) {
+            System.out.println("Cliente: " + nome+",ID:"+id + " está com assinatura ativa.");
+        } else {
+            System.out.println("Cliente: " + nome+",ID:"+id + " não tem assinatura.");
+        }
+    }
 }
